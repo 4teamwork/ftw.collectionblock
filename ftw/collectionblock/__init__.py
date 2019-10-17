33b6ea31d95320dea2310a42e04bf5d2341ce7f5
Matthias Osswald <m@osswald.li>
2019-10-17 14:43:24 +0200

    Add plone 5.1 support.

--- ftw/collectionblock/__init__.py
+++ ftw/collectionblock/__init__.py
@@ -2,3 +2,8 @@ from zope.i18nmessageid import MessageFactory
 
 
 _ = MessageFactory('ftw.collectionblock')
+
+
+def initialize(context):
+    """Required for being able to uninstall this product.
+    """
